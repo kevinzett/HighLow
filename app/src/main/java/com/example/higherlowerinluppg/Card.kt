38e8e8702dbd699fdab5942120af675ae1741c2b
Kevin Zetterlind <kevinzetterlind@Kevins-MacBook-Air.local>
2021-09-22 08:34:59 +0200

    created a newActivity

--- app/src/main/java/com/example/higherlowerinluppg/Card.kt
+++ app/src/main/java/com/example/higherlowerinluppg/Card.kt
@@ -1,7 +1,15 @@
 package com.example.higherlowerinluppg
 
+import android.view.View
 import android.widget.ImageView
+import java.util.*
 
-class Card(var value: Int, var image: ImageView) {
+class Card(var value: Int, var image: Int) {
+
+
+}
+
+    fun onHigh(view: View) {
+
 
 }
--- app/src/main/java/com/example/higherlowerinluppg/MainActivity.kt
+++ app/src/main/java/com/example/higherlowerinluppg/MainActivity.kt
@@ -2,18 +2,85 @@ package com.example.higherlowerinluppg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import java.lang.Math.random
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageView: ImageView
+    lateinit var textView: TextView
+
+    val cardList = listOf<Card>(Card(2, R.drawable.two),
+        Card(3, R.drawable.three),
+        Card(4, R.drawable.four),
+        Card(5, R.drawable.five),
+        Card(6, R.drawable.six),
+        Card(7, R.drawable.seven),
+        Card(8, R.drawable.eigth),
+        Card(9, R.drawable.nine),
+        Card(10, R.drawable.ten),
+        Card(11, R.drawable.knekt),
+        Card(12, R.drawable.dam),
+        Card(13, R.drawable.kung),
+        Card(1, R.drawable.ess))
+
+    var randomCard = cardList.random()
+    var cardValue = randomCard.value
+    var cardImage = randomCard.image
+    var currentScore = 0
+    var newCardValue = randomCard.value
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         imageView = findViewById(R.id.imageView)
+        textView = findViewById(R.id.textView)
 
-        val cardList = mutableListOf<Card>()
 
-        var two = Card(2, imageView)
+        newCard()
+
+        //val currentCard =
+
+
 
     }
+
+    fun newCard(){
+        randomCard = cardList.random()
+        cardValue = randomCard.value
+        cardImage = randomCard.image
+        imageView.setImageResource(cardImage)
+
+    }
+
+    fun onHigher(view: View){
+        val newCardValue = randomCard.value
+        newCard()
+        //newCardValue = randomCard.value
+        if(cardValue >= newCardValue) {
+            currentScore++
+            textView.text = currentScore.toString()
+        } else {
+            currentScore--
+            textView.text = currentScore.toString()
+        }
+    }
+
+    fun onLower(view: View) {
+        val newCardValue = randomCard.value
+        newCard()
+        if(cardValue <= newCardValue) {
+            currentScore++
+            textView.text = currentScore.toString()
+        } else {
+            currentScore--
+            textView.text = currentScore.toString()
+        }
+    }
+
+
+
+
 }
